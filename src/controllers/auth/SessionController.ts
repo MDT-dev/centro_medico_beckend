5d67cc23e95dc46bd7578473e62ce46ea5c4187f
Ananiassapalodeveloper <ananiassapalot@gmail.com>
2026-04-23 10:32:42 +0100

    added again

--- src/controllers/auth/SessionController.ts
+++ src/controllers/auth/SessionController.ts
@@ -3,7 +3,6 @@ import { SessionService } from "../../services/auth/SessionService";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-
 export class SessionController {
   async handle(req: Request, res: Response, next: NextFunction) {
     try {
@@ -13,10 +12,10 @@ export class SessionController {
 
       res.cookie("auth_token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        secure: true, // 🔥 sempre true em produção
+        sameSite: "none", // 🔥 ESSENCIAL
         maxAge: 24 * 60 * 60 * 1000,
-        path: "/"
+        path: "/",
       });
 
       return res.json({ message: "Login successful", user });
@@ -24,6 +23,4 @@ export class SessionController {
       next(err);
     }
   }
-
 }
-
